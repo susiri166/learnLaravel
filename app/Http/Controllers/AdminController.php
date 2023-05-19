c323cb83a0bbd7c63baaa9b4904f2d458dae15ce
susiri166 <63943099+susiri166@users.noreply.github.com>
2023-05-19 08:59:40 +0530

    Initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+    public function AdminDashboard(){
+        return view('admin.admin');
+    }
+
+
+}
